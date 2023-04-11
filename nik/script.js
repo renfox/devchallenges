9fc5f9f7b59fafcd21552c5ec40ab835cc974a7d
renfox <aleksio2011@gmail.com>
2023-04-11 23:12:53 +0300

    nik 15%

--- nik/script.js
+++ nik/script.js
@@ -3,25 +3,44 @@ const headerClass = document.querySelector('header');
 const aboutMe = document.querySelector('#aboutMe');
 let intervalId;
 let e = 0;
-playHeader.addEventListener('click', function() { 
-    const bgBox = document.querySelector('.box'); 
-    headerClass.classList.add("anim-header");
-    playHeader.textContent = '';
-    bgBox.style.backgroundImage = "url(/nik/img/nik.jpg)"; 
-    
-    function sayHi(id, cls) {
-        const hi = document.querySelector(id);
-        hi.removeAttribute('hidden');
-        hi.classList.add(cls);
-    }
-    setTimeout(function() { sayHi('#hello', 'anim-hello'); }, 9000);
-    setTimeout(function() { sayHi('#myName', 'anim-name'); }, 14000);
-    
+let b = 0;
+
+function sayHi(id, cls) {
+    const ids = document.querySelector(id);
+    ids.removeAttribute('hidden');
+    ids.classList.add(cls);
+}
+
+function OpsMinus(elem,interval = 10, times) {
     function isOps() { 
         // увеличиваем счетчик на 0.1 каждый раз       
-        e += 0.3;
+        e -= 0.1;
         // применяем к стилям элемента
-        aboutMe.style.opacity = e;
+        elem.style.opacity = e;
+        if (e <= 0) {
+          // Останавливаем анимацию, очистив интервал
+          clearInterval(intervalId);
+        }        
+      }
+      // оборачиваем в setTimeout чтобы функция запустилась после нужной задержки
+      setTimeout(function() {
+        // Останавливаем анимацию, если она уже запущена
+        clearInterval(intervalId);
+        // Сбрасываем положение счетчика
+        e = 1;
+        // Вызываем функции isOps() каждую 1 секунду и сохраняем идентификатор интервала в переменной
+        intervalId = setInterval(isOps, interval );
+        return intervalId;
+      }, times);
+}
+
+function OpsPlus(elem, interval, times) {
+    function isOps() { 
+        elem.removeAttribute('hidden');
+        // увеличиваем счетчик на 0.1 каждый раз       
+        e += 0.1;
+        // применяем к стилям элемента
+        elem.style.opacity = e;
         if (e >= 1) {
           // Останавливаем анимацию, очистив интервал
           clearInterval(intervalId);
@@ -34,26 +53,37 @@ playHeader.addEventListener('click', function() {
         // Сбрасываем положение счетчика
         e = 0;
         // Вызываем функции isOps() каждую 1 секунду и сохраняем идентификатор интервала в переменной
-        intervalId = setInterval(isOps, 1000);
-      }, 18000);
+        intervalId = setInterval(isOps, interval);
+      }, times);
+}
 
+playHeader.addEventListener('click', function() { 
+    const bgBox = document.querySelector('.box'); 
+    headerClass.classList.add("anim-header");
+    playHeader.textContent = '';
+    bgBox.style.backgroundImage = "url(/nik/img/nik.jpg)"; 
     
+    
+    setTimeout(function() { sayHi('#hello', 'anim-text'); }, 9000);
+    setTimeout(function() { sayHi('#myName', 'anim-text'); }, 14000);
+    
+    OpsPlus(aboutMe, 10, 18000);    
       
 })
 
 aboutMe.addEventListener('click', function() {    
-const iHidden = document.querySelector('#hello');
-const isReplace = document.querySelector('#myName');
-    isReplace.innerHTML = isReplace.innerHTML.replace(/^.*Андра/, "Андра");
-    isReplace.classList.add("isReplace");
-    iHidden.classList.add("isOpacity");
+const hello = document.querySelector('#hello');
+const myName = document.querySelector('#myName');
+    myName.innerHTML = myName.innerHTML.replace(/^.*Андра/, "Андра");
+    myName.classList.add("isReplace");
+    hello.classList.add("isOpacity");
     
-    function isOps() { 
+    function isTop() { 
         // увеличиваем счетчик на 0.1 каждый раз       
         e -= 1;
         // применяем к стилям элемента
-        aboutMe.style.top = e + '%';
-        if (e >= 6) {
+        myName.style.top = e + '%';
+        if (e <= 6) {
           // Останавливаем анимацию, очистив интервал
           clearInterval(intervalId);
         }
@@ -63,9 +93,49 @@ const isReplace = document.querySelector('#myName');
         // Останавливаем анимацию, если она уже запущена
         clearInterval(intervalId);
         // Сбрасываем положение счетчика
+        e = 12;
+        // Вызываем функции isOps() каждую 1 секунду и сохраняем идентификатор интервала в переменной
+        intervalId = setInterval(isTop, 10);
+        return intervalId;
+      }, 4000);
+
+      OpsMinus(aboutMe, 10,  6000)
+
+    /*  function isOps() { 
+        const likeCoffee = document.querySelector('.likeCoffee')
+        // увеличиваем счетчик на 0.1 каждый раз       
+        e += 1;
+        // применяем к стилям элемента
+        likeCoffee.style.opacity = e;
+        if (e >= 1 ) {
+          // Останавливаем анимацию, очистив интервал
+          clearInterval(intervalId);
+        }               
+      }
+      // оборачиваем в setTimeout чтобы функция запустилась после нужной задержки
+      setTimeout(function() {
+        // Останавливаем анимацию, если она уже запущена
+        clearInterval(intervalId);
+        // Сбрасываем положение счетчика
         e = 0;
         // Вызываем функции isOps() каждую 1 секунду и сохраняем идентификатор интервала в переменной
-        intervalId = setInterval(isOps, 500);
-      }, 20000);
+        intervalId = setInterval(isOps, 30);
+        return intervalId;
+      }, 6000); */
+      setTimeout(function() { sayHi('#likeCoffee', 'anim-text'); }, 8000);
+      setTimeout(function() { sayHi('#likeCoffee', 'anim-text30'); }, 8000);
+      setTimeout(function(){
+        document.querySelector('#likeCoffee').classList.add("trCoffee");
+      }, 13000);
 
-})
+      const bgCofee = document.querySelector('.coffee');
+      OpsPlus(bgCofee, 100, 19000)
+
+      setTimeout(function() { sayHi('#likeAnime', 'anim-text'); }, 24000);
+      setTimeout(function() { sayHi('#likeAnime', 'anim-text20'); }, 24000);
+      setTimeout(function(){
+        document.querySelector('#likeAnime').classList.add("trAnime");
+      }, 26000);
+      const bgAnime = document.querySelector('.anime');
+      OpsPlus(bgAnime, 100, 31000)
+});
